2d673cdf0603f05afd74757455e98afd05f37e20
Nicole Quenamá <110855023+nicoleQuenama@users.noreply.github.com>
2026-05-27 21:49:37 -0400

    error validation.java

--- src/test/java/com/swag/pe/pages/validations/ValidationPage.java
+++ src/test/java/com/swag/pe/pages/validations/ValidationPage.java
@@ -5,7 +5,7 @@ import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.support.FindBy;
 
 public class ValidationPage extends PageObject {
-    @FindBy(xpath = "//div[@class='product_label']")
+    @FindBy(xpath = "//span[@class='title']")
     protected WebElementFacade lbl_product;
     @FindBy(css="h3[data-test='error']")
     protected WebElementFacade lbl_errorMessage;
